9742a8508c85f74058200a47af3fad83a0aa7807
fran <o1157108955@gmail.com>
2024-12-16 12:51:47 +0800

    add launch file

--- src/controller.cpp
+++ src/controller.cpp
@@ -13,10 +13,10 @@ ros::Subscriber state_sub;
 double mq = 0.33;
 double g = 9.81;
 double kp = 7.65, kv = 3.6, kr = 120.0, hr = 30.0;
-VectorXd p1 = (VectorXd(3) << -1.11674940231383e-06, 0.00216553259709083, -0.0276877882890898).finished();
-VectorXd p2 = (VectorXd(3) << 0.000413683067484175, 0.932875787982657, 0).finished();
-VectorXd p3 = (VectorXd(3) << 0.000343767264533347,1.05504806357145,0).finished();
-VectorXd p4 = (VectorXd(3) << 0.000770252131194202,0.866138866957217,0).finished();
+Vector3d p1(-1.11674940231383e-06, 0.00216553259709083, -0.0276877882890898);
+Vector3d p2(0.000413683067484175, 0.932875787982657, 0);
+Vector3d p3(0.000343767264533347,1.05504806357145,0);
+Vector3d p4(0.000770252131194202,0.866138866957217,0);
 bool use_polyval = true;
 
 // 限制函数
@@ -79,13 +79,20 @@ void Controller(double& T, Vector3d& omega, const Vector3d& pd, const Vector3d& 
     Vector3d r3 = R*e3;
     T = Td*r3d.dot(r3);
     // ROS_INFO("r3d: %f %f %f", r3d(0), r3d(1), r3d(2));
-    // ROS_INFO("r3: %f %f %f", r3(0), r3(1), r3(2));
+    Vector3d e2(0, 1, 0);
+    Vector3d r2 = R*e2;
+    ROS_INFO("r2: %f %f %f", r2(0), r2(1), r2(2));
+    Vector3d e1(1, 0, 0);
+    Vector3d r1 = R*e1;
+    ROS_INFO("r1: %f %f %f", r1(0), r1(1), r1(2));
+    ROS_INFO("r3: %f %f %f", r3(0), r3(1), r3(2));
     Vector3d F = -T*R*e3;
     Vector3d dzv = F/mq + g*e3 - d2pd;
     Vector3d dFd = mq*(-kp*zv - kv*dzv + d3pd);
     Vector3d dr3d = S(r3d)*S(r3d)*dFd/Fd.norm();
     Vector3d zr = r3 - r3d;
-    omega = - S(e3)*S(e3)*(R.transpose()*S(r3d)*dr3d + kr/hr*S(e3)*R.transpose()*r3d + Td/(mq*hr)*S(e3)*R.transpose()*(b*zp+zv)) - 1.5*abs(yaw)*e3;
+    omega = - S(e3)*S(e3)*(R.transpose()*S(r3d)*dr3d + kr/hr*S(e3)*R.transpose()*r3d + Td/(mq*hr)*S(e3)*R.transpose()*(b*zp+zv)) - 1.5*(yaw-1.57)*e3;
+    // ROS_INFO("yaw: %f", yaw);
 
 
     
@@ -102,6 +109,7 @@ void controllCallback(const test_controller::UAVState::ConstPtr& state_msg) {
                 state_msg->attitude.z
             );
             Matrix3d R = attitude.toRotationMatrix();
+            Vector3d euler = attitude.toRotationMatrix().eulerAngles(2, 1, 0);
             Vector3d e3(0, 0, 1);
 
 
@@ -113,7 +121,7 @@ void controllCallback(const test_controller::UAVState::ConstPtr& state_msg) {
             Vector3d omega;
 
             // 调用控制算法
-            Controller(T, omega, pd, dpd, d2pd, d3pd, position, velocity, R, 0.0,
+            Controller(T, omega, pd, dpd, d2pd, d3pd, position, velocity, R, euler(0),
                        mq, g, e3, kp, kv, kr, hr);
 
             // 发布控制指令
@@ -121,8 +129,8 @@ void controllCallback(const test_controller::UAVState::ConstPtr& state_msg) {
             if(use_polyval) {
                 command_msg.thrust = T;
                 // command_msg.thrust = clamp(polyval(T,p1),0,1);
-                command_msg.omega.x = -clamp(sign(omega(0))*polyval(abs(omega(0)*180/3.141592654), p2),-360,360);
-                command_msg.omega.y = -clamp(sign(omega(1))*polyval(abs(omega(1)*180/3.141592654), p3),-360,360);
+                command_msg.omega.x = clamp(sign(omega(0))*polyval(abs(omega(0)*180/3.141592654), p2),-360,360);
+                command_msg.omega.y = clamp(sign(omega(1))*polyval(abs(omega(1)*180/3.141592654), p3),-360,360);
                 command_msg.omega.z = clamp(0*sign(omega(2))*polyval(abs(omega(2)*180/3.141592654), p4),-180,180);
                 command_pub.publish(command_msg);
             }
@@ -133,7 +141,7 @@ void controllCallback(const test_controller::UAVState::ConstPtr& state_msg) {
                 command_msg.omega.z = omega(2);
                 command_pub.publish(command_msg);
             }
-            ROS_INFO("Published command: thrust = %f, omega = [%f, %f, %f]", T, command_msg.omega.x, command_msg.omega.y, command_msg.omega.z);
+            // ROS_INFO("Published command: thrust = %f, omega = [%f, %f, %f]", T, command_msg.omega.x, command_msg.omega.y, command_msg.omega.z);
 
         }
 
